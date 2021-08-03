df28b8670d0a96bfcc68274eac98848d893f3b47
Demarcus Lester <demarcuslester12@gmail.com>
2021-08-03 18:20:30 -0500

    Ex 1,2,3 completed

---
+++ main.js
@@ -0,0 +1,36 @@
+// console.log("sanity");
+
+// EX 1
+// 1. Input and Loops
+// Create two variables called five assigned 5 and eight assigned 8. 
+// Next create an if statement compairing five is less than eight. If it's true, , print “5 is less than 8”. Else, print ERROR in the console or browser.
+// i will need two variables
+//i will need an if statement with a true conditional
+// i believe i can use a while loop here
+//first we need to create two varibles with these names and values
+// let five = 5;
+// let eight = 8;
+// //create an if statement comparing the two variables
+// if (five < eight) { //If its true it will print the 1st option
+//     console.log('5 is less than 8');
+// } else { //if false it will print error
+//     console.log(" ERROR . . Try Again ")
+// }
+
+
+// EX 2
+// Print all even numbers from a user's given start and stop number in the console or browser.
+//I belive i will need a function to gather the two start and stop points
+//then we will need to put into an array
+//then we will need to take out all the values that return as even using modulous
+//then we will need print those to for loop
+
+
+function ex2() { // let get our two variables
+    let userStart = prompt(" Enter a number for your start point ");
+    let userEnd = prompt(" Enter a number for your end point ");
+
+    let 
+
+}
+console.log(userStart,userEnd);
